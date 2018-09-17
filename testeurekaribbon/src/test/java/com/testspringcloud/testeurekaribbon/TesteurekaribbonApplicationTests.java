e3cd8c7744433e3953065f6b52c6594163694914
zhoupei@yktour.com.cn <zhoupei@yktour.com.cn>
2018-09-17 10:32:17 +0800

    Initial commit

---
+++ testeurekaribbon/src/test/java/com/testspringcloud/testeurekaribbon/TesteurekaribbonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.testspringcloud.testeurekaribbon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TesteurekaribbonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ testeurekaserver/src/main/java/com/zz/testspringcloud/testeurekaserver/TesteurekaserverApplication.java
@@ -0,0 +1,14 @@
+package com.zz.testspringcloud.testeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TesteurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteurekaserverApplication.class, args);
+	}
+}
